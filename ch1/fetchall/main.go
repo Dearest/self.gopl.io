820df55d6a1abc0d58b00606958f689c9a412ff8
dearest <iotu.hanchen@gmail.com>
2018-04-03 16:53:23 +0800

    ch1 goriutine

---
+++ ch1/fetchall/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapse\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
+
+// go run main.go https://www.zhihu.com/ http://gopl.io http://www.baidu.com
+// 0.23s  114841 http://www.baidu.com
+// 0.77s    4154 http://gopl.io
+// 0.90s   24471 https://www.zhihu.com/
+// 0.90s elapse
